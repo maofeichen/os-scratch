e4d9bc1d3a89222e8f84cbeea59cdd534e90244e
mchen <mfchen@outlook.com>
2019-06-21 23:40:43 -0400

    Add 02 boot sect hello asm, print on screen.

---
+++ 02-boot_sect_hello.asm
@@ -0,0 +1,19 @@
+; Reference from:
+;   https://github.com/cfenollosa/os-tutorial/tree/master/02-bootsector-print
+
+mov ah, 0x0e ; tty mode
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10 ; 'l' is still on al, remember?
+mov al, 'o'
+int 0x10
+
+jmp $ ; jump to current address = infinite loop
+
+; padding and magic number
+times 510 - ($-$$) db 0
+dw 0xaa55 
